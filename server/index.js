6b38a578e2fcec32d01a421b89ee419f9cc7b631
Thomas Lowry <minilao94@yahoo.com>
2018-05-25 03:45:14 +0000

    updated server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
-app.post('/api/console', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-});
+// app.use(cors());
+// app.post('/api/console', (req, res) => {
+//     console.log(req.body);
+//     res.sendStatus(200);
+// });
+app.use(express.static(__dirname + '/../build'));
 app.listen(3015, () => console.log('collin listening on port 3015!'));
